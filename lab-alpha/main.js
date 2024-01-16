27db618dcb8c26aaf7bbbccc02007e8edbe4e4c8
hrantgartley <doyouevenjump@gmail.com>
2024-01-16 13:12:39 -0600

    reverted changes

--- lab-alpha/main.js
+++ lab-alpha/main.js
@@ -15,6 +15,12 @@ $(function () {
     function submitForm() {
         const randomPlace = places[Math.floor(Math.random() * places.length)]
         let fname = $("#fname").val()
+        let validUsername = /^[a-zA-Z]+$/.test(fname)
+        if (!validUsername) {
+            $("#something").text("Please enter a valid name").fadeIn(2000)
+            submitForm()
+            return
+        }
         if (fname === "") {
             fname = "Mysterious Traveler"
         } else {
