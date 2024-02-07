83e1476d1d0faac02689ff51cd904afc7eb59626
hrantgartley <doyouevenjump@gmail.com>
2024-02-07 17:48:52 -0600

    charlie added

---
+++ improved-train-charlie/scripts/script.js
@@ -0,0 +1,29 @@
+/* global $ */
+$(function () {
+    let wowCount = 0
+    let wowMsg
+
+    $("#dogePic").draggable({
+        scroll: false,
+
+        start: function () {
+            wowCount++
+
+            if (wowCount <= 4) {
+                wowMsg = "your wow is weak"
+                // set background color to red
+                $("body").css("background-color", "red")
+            } else if (wowCount <= 10) {
+                wowMsg = "your wow is mediocre"
+                $("body").css("background-color", "yellow")
+            } else {
+                wowMsg = "your wow is extraordinary"
+                $("body").css("background-color", "blue")
+            }
+
+            $("#wowOutput").text(wowMsg + "(" + wowCount + ")")
+        },
+    })
+
+    $("#dogeDiv").draggable()
+})
