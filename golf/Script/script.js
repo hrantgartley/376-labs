409eb5e73456b21ad9816e2c8f73ac38899001d4
hrantgartley <doyouevenjump@gmail.com>
2024-03-16 22:46:29 -0500

    not working --help

--- golf/Script/script.js
+++ golf/Script/script.js
@@ -1,144 +1,38 @@
 $(function () {
-  const games = [
-    {
-      title: "improved-octo-chainsaw-charlie, verbose-guacamole-delta ",
-      link: "https://devan4264.github.io/improved-octo-chainsaw-charlie/",
-      link2: "https://devan4264.github.io/verbose-guacamole-delta/",
-      author: "devan4264",
-    },
-    {
-      title: "legendary-goggles-charlie, delta-jubilant-doodle",
-      link: "https://mackenzie24t.github.io/legendary-goggles-charlie/",
-      link2: "https://mackenzie24t.github.io/delta-jubilant-doodle/",
-      author: "mackenzie24t",
-    },
-    {
-      title: "crispy-charlie, decatur-delta",
-      link: "https://johnallenb.github.io/crispy-charlie-lab03-blaxton/",
-      link2: "https://johnallenb.github.io/decatur-delta-lab04-blaxton/",
-      author: "JohnAllenB",
-    },
-    {
-      title: "turbo-pancake-charlie, fluffy-pancake-charlie",
-      link: "https://jacob-vance.github.io/turbo-pancake-charlie/",
-      link2: "https://jacob-vance.github.io/fluffy-pancake-delta/",
-      author: "Jacob-Vance",
-    },
-    {
-      title: "delta-jubilant-octo-winner, fictional-broccoli-charlie",
-      link: "https://jaydengreen23.github.io/delta-jubilant-octo-winner/",
-      link2: "https://jaydengreen23.github.io/fictional-broccoli-charlie/",
-      author: "Jayden Green",
-    },
-    {
-      title: "Hot or Cold, Name Game",
-      link: "https://smaldonado1326.github.io/cuddly-invention--charlie/",
-      link2: "https://smaldonado1326.github.io/glowing-adventure-delta/",
-      author: "Sam Maldonado",
-    },
-    {
-      title: "Charlie Meme, Delta Names",
-      link: "https://nrobinson4.github.io/bug-free-potato-charlie/",
-      link2: "https://nrobinson4.github.io/cool-animal-delta/",
-      author: "Nolen Robinson",
-    },
-    {
-      title: "Llama Game, Doge Game",
-      link: "https://michaelhaydenduran.github.io/chica-bonita-delta/",
-      link2: "https://michaelhaydenduran.github.io/silly-dino-charlie/",
-      author: "Michael Hayden Duran",
-    },
-    {
-      title: "charlie-project-alex, delta-project-alex",
-      link: "https://pglenn1.github.io/charlie-project-alex/",
-      link2: "https://pglenn1.github.io/delta-project-alex/",
-      author: "Alex Glenn",
-    },
-    {
-      title: "BravoSuMuchWow, Animal Name Game",
-      link: "https://hkevin555.github.io/lab-charlie/",
-      link2: "https://hkevin555.github.io/DeltaLab/",
-      author: "Kevin Hernandez",
-    },
-    {
-      title: "Delta Lab Spirit Animal",
-      link: "https://marvin-2056.github.io/Delta-CIS-367/",
-      link2: "#",
-      author: "Marvin De La Cruz",
-    },
-    {
-      title: "nesia-charlie-lab, currentDeltaLab",
-      link: "https://nesiagoodloe.github.io/nesia-charlie-lab/",
-      link2: "https://nesiagoodloe.github.io/currentDeltaLab/",
-      author: "Rodnesia Goodloe",
-    },
-    {
-      title: "Charlie, Delta",
-      link: "https://cm1803.github.io/charlie/",
-      link2: "https://cm1803.github.io/delta/",
-      author: "Claire Mitchell",
-    },
-    {
-      title: "Hopping Zone Charlie, Spirit Animal Delta",
-      link: "https://mmandelyn.github.io/silver-seal-charlie/",
-      link2: "https://mmandelyn.github.io/pink-panda-delta/",
-      author: "Mandy McGinnis",
-    },
-    {
-      title: "super-adventure-charlie, animated-sniffle-foxtrot",
-      link: "https://skysaunders.github.io/animated-sniffle-foxtrot/",
-      link2: "https://skysaunders.github.io/super-adventure-charlie/",
-      author: "Skyler Saunders",
-    },
-    {
-      title: "Glowing Meme Foxtrot, Glowing Gigle Echo Lab",
-      link: "https://cbuf0rd.github.io/glowing-meme-foxtrot/",
-      link2: "https://cbuf0rd.github.io/glowing-giggle-echo-lab/",
-      author: "Candice Buford",
-    },
-    {
-      title: "charlie, delta",
-      link: "https://hrantgartley.github.io/didactic-waffle-delta/",
-      link2: "https://hrantgartley.github.io/jubilant-bassoon-echo/",
-      author: "Grant Hartley",
-    },
-    {
-      title: "probable-charlie, turbo-delta",
-      link: "https://quantumcoder-242.github.io/probable-charlie/",
-      link2: "https://quantumcoder-242.github.io/turbo-delta/",
-      author: "Navraj_Singh",
-    },
-    {
-      title: "psychic-delta, charlie-lab",
-      link: "https://akashdeepsingh0047.github.io/psychic-delta/",
-      link2: "https://akashdeepsingh0047.github.io/Chrlie-lab-By-Akashdeep/",
-      author: "AkashdeepSingh0047",
-    },
-  ];
-  // iterate over the JSON array
-  $.each(games, function (index, item) {
-    console.log("Game Title: " + item.title);
-    console.log("Game URL1: " + item.link);
-    console.log("Game URL1: " + item.link2);
-    console.log("Game Author: " + item.author);
+    // Fetch the JSON data asynchronously
+    $.getJSON("../data/games.json", function (games) {
+        console.log("JSON Data:", games); // Check if JSON data is loaded correctly
 
-    // create a brand new HTML element JUST with code
+        // Iterate over the JSON array
+        $.each(games, function (index, item) {
+            console.log("Item:", item); // Check each item in the iteration
 
-    var el = `<div class='card text-center mx-auto bg-dark blue' > \
-      <button class='cardOpen2 btn btn-outline-info'>Games: ${item.title} </button> \
-       <div class='cardHide'> \
-           <br> \
-           <a href="${item.link}" target="_blank" class="link-info" >GAME LINK1</a> \
-           <a href="${item.link2}" target="_blank" class="link-info" >GAME LINK2</a> \
-           <p class="text-white">Created by: ${item.author} </p> \
-           <br> \
-       </div>
-     </div>`;
+            // Iterate over the array of objects within the item array
+            $.each(item, function (innerIndex, innerItem) {
+                // Create HTML elements for each JSON object
+                const el = `<div class="card text-center mx-auto bg-dark text-blue" style="width: 18rem;">
+                    <button class="cardOpen2 btn btn-outline-info">Games: ${innerItem.title}</button>
+                    <div class="cardHide">
+                        <br>
+                        <a href="${innerItem.link1}" target="_blank" class="link-info btn btn-primary">GAME LINK1</a>
+                        <a href="${innerItem.link2}" target="_blank" class="link-info btn btn-primary">GAME LINK2</a>
+                        <p class="text-white">Created by: ${innerItem.author}</p>
+                        <br>
+                    </div>
+                </div>`;
 
-    console.log(el);
+                console.log("Generated HTML:", el); // Check generated HTML
 
-    $("#firstCard").append(el);
+                // Append the HTML element to the container
+                $("#firstCard").append(el);
 
-    $(".nav-dropdown").append($(`<li><a href="#!"> ${item.author} </a></li>`));
-  });
+                // Append the author to the navigation dropdown
+                $(".nav-dropdown").append(
+                    $(
+                        `<li><a href="#!" class="dropdown-item">${innerItem.author}</a></li>`,
+                    ),
+                );
+            });
+        });
+    });
 });
--- lab-alpha/main.js
+++ lab-alpha/main.js
@@ -2,37 +2,37 @@
 /* eslint-disable no-undef */
 $(function () {
     $("#submit-btn").on("click", function () {
-        submitForm()
-    })
+        submitForm();
+    });
 
     $("#fname").on("keydown", function (e) {
         if (e.which === 13) {
-            submitForm()
+            submitForm();
         }
-    })
+    });
 
     function submitForm() {
-        const randomPlace = places[Math.floor(Math.random() * places.length)]
-        let fname = $("#fname").val()
-        let validUsername = /^[a-zA-Z]+$/.test(fname)
+        const randomPlace = places[Math.floor(Math.random() * places.length)];
+        let fname = $("#fname").val();
+        let validUsername = /^[a-zA-Z]+$/.test(fname);
         if (!validUsername) {
-            $("#something").text("Please enter a valid name").fadeIn(2000)
-            submitForm()
-            return
+            $("#something").text("Please enter a valid name").fadeIn(2000);
+            submitForm();
+            return;
         }
         if (fname === "") {
-            fname = "Mysterious Traveler"
+            fname = "Mysterious Traveler";
         } else {
-            fname = fname.charAt(0).toUpperCase() + fname.slice(1)
+            fname = fname.charAt(0).toUpperCase() + fname.slice(1);
         }
         let greeting =
-            "Hello " + fname + "! What's up! Welcome to " + randomPlace
+            "Hello " + fname + "! What's up! Welcome to " + randomPlace;
 
-        $("#something").text(greeting).fadeIn(2000).replaceWith("")
+        $("#something").text(greeting).fadeIn(2000).replaceWith("");
     }
-})
+});
 
 function resetForm() {
-    $("#fname").val("")
-    $("#something").fadeOut(2000)
+    $("#fname").val("");
+    $("#something").fadeOut(2000);
 }
